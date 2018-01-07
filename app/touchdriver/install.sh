a27559bdd40f12ceb0676dee240843a62057ec3e
Chris <chris2fourlaw@gmail.com>
2018-01-07 15:27:01 -0800

    fix path, and correctly load display drivers

--- app/touchdriver/install.sh
+++ app/touchdriver/install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/sh
 
 # Copy driver folder and change permissions
-cp -r ../touchdriver/ /etc/opt/elo-usb
+cp -r /usr/src/app/touchdriver/ /etc/opt/elo-usb
 cd /etc/opt/elo-usb
 chmod 777 *
 chmod 444 *.txt
 cp /etc/opt/elo-usb/99-elotouch.rules /etc/udev/rules.d
 
 # Startup Driver
-./elousbd
+sh ./loadEloTouchUSB.sh
 ./eloautocalib --renew
--- app/touchdriver/loadEloTouchUSB.sh
+++ app/touchdriver/loadEloTouchUSB.sh
@@ -3,9 +3,5 @@
 # Create Elo Devices for communication
 /etc/opt/elo-usb/eloCreateFifo.sh
 
-# Load the PC speaker kernel module into memory for Beep-On-Touch 
-modprobe pcspkr
-sleep 1
-
-# Load the Elo USB Touchscreen Driver Daemon into memory 
+# Load the Elo USB Touchscreen Driver Daemon into memory
 /etc/opt/elo-usb/elousbd --xwarppointer
